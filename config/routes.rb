b23a72949604501a4eb3c92ca2649b96cc5ceb2d
Hugo-Vidal <hugo.f.vidal.r@gmail.com>
2019-06-17 18:40:31 -0400

    Integration Bootstrap, SASS

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  
   get 'pages/home'
   get 'pages/about'
+  root 'pages#home'
+  resources :products
+  get 'products/index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
